45f5e7906bad315d6cfc183d333c85ff4e008373
YAMAMOTO Takashi <yamamoto@midokura.com>
2025-07-07 18:24:19 +0900

    hack for wasi

--- library/net_sockets.c
+++ library/net_sockets.c
@@ -159,7 +159,7 @@ int mbedtls_net_bind(mbedtls_net_context *ctx, const char *bind_ip, const char *
     hints.ai_socktype = proto == MBEDTLS_NET_PROTO_UDP ? SOCK_DGRAM : SOCK_STREAM;
     hints.ai_protocol = proto == MBEDTLS_NET_PROTO_UDP ? IPPROTO_UDP : IPPROTO_TCP;
     if (bind_ip == NULL) {
-        hints.ai_flags = AI_PASSIVE;
+        bind_ip = "0.0.0.0";
     }
 
     if (getaddrinfo(bind_ip, port, &hints, &addr_list) != 0) {
