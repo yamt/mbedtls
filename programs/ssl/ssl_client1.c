cd10c774ea8d95e131cfe515ac95070c3c33536a
YAMAMOTO Takashi <yamamoto@midokura.com>
2025-07-15 14:06:51 +0900

    ssl_client1.c: hardcode the name of my test host

--- programs/ssl/ssl_client1.c
+++ programs/ssl/ssl_client1.c
@@ -33,7 +33,8 @@ int main(void)
 #include <string.h>
 
 #define SERVER_PORT "4433"
-#define SERVER_NAME "localhost"
+//#define SERVER_NAME "localhost"
+#define SERVER_NAME "tadpole.testlan"
 #define GET_REQUEST "GET / HTTP/1.0\r\n\r\n"
 
 #define DEBUG_LEVEL 1
